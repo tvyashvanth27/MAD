46f774cadee061e1843292cfbfecb15d2ab2fc5a
Y A S H <110223808+tvyashvanth27@users.noreply.github.com>
2024-05-09 03:12:59 -0500

    Add files via upload

---
+++ Assignment1/assignment1-master/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val countTextView: TextView = findViewById(R.id.countTextView)
+        val checkInButton: Button = findViewById(R.id.checkInButton)
+        val checkOutButton: Button = findViewById(R.id.checkOutButton)
+
+        checkInButton.setOnClickListener {
+            count++
+            countTextView.text = count.toString()
+        }
+
+        checkOutButton.setOnClickListener {
+            if (count > 0) {
+                count--
+                countTextView.text = count.toString()
+            }
+        }
+    }
+}
